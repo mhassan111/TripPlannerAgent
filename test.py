0a489d96728ac1a61306b2d31b3b2bfb97c7cea1
mhassan111 <mhassanuetcs12@gmail.com>
2026-07-14 14:45:38 +0500

    flight and travily tools

---
+++ test.py
@@ -0,0 +1,7 @@
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+
+# res = tavily_search("Best hotels in Lahore")
+# print(res)
+
+print(search_flights("Plan a 7 days Japan trip from Bangladesh"))
